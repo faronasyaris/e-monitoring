6badcd0b03c73725bcc5f07e92b386bcf8e22837
Robby Gustian <robbygustian11@gmail.com>
2022-08-23 10:56:58 +0700

    fix add program

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,54 +15,54 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name'=>'Kepala Dinas',
-            'email'=>'kepaladinas@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Kepala Dinas',
+            'name' => 'Kepala Dinas',
+            'email' => 'kepaladinas@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Kepala Dinas',
         ]);
         User::create([
-            'name'=>'Sekretaris',
-            'email'=>'sekretaris@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Sekretaris',
-        ]);
-
-        User::create([
-            'name'=>'employee',
-            'email'=>'employee@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Employee',
-            'field_id'=>1
+            'name' => 'Sekretaris',
+            'email' => 'sekretaris@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Sekretaris',
         ]);
 
         User::create([
-            'name'=>'kepala bidang 1',
-            'email'=>'kepalabidang1@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Kepala Bidang',
-            'field_id'=>1
+            'name' => 'employee',
+            'email' => 'employee@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Employee',
+            'field_id' => 1
         ]);
 
         User::create([
-            'name'=>'kepala bidang 2',
-            'email'=>'kepalabidang2@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Pelaksana',
-            'field_id'=>2
+            'name' => 'kepala bidang 1',
+            'email' => 'kepalabidang1@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Kepala Bidang',
+            'field_id' => 1
         ]);
 
         User::create([
-            'name'=>'kepala bidang 3',
-            'email'=>'kepalabidang3@gmail.com',
-            'password'=>bcrypt('password'),
-            'NIP'=>'123456',
-            'role'=>'Kepala Bidang',
-            'field_id'=>3
+            'name' => 'kepala bidang 2',
+            'email' => 'kepalabidang2@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Kepala Bidang',
+            'field_id' => 2
+        ]);
+
+        User::create([
+            'name' => 'kepala bidang 3',
+            'email' => 'kepalabidang3@gmail.com',
+            'password' => bcrypt('password'),
+            'NIP' => '123456',
+            'role' => 'Kepala Bidang',
+            'field_id' => 3
         ]);
     }
 }
